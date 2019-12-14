14045698c089dc92d07d5f75742437022fdede1a
MoraPresence <od34156la@mail.ru>
2019-12-14 03:41:21 +0300

    version 1.0

--- include/header.hpp
+++ include/header.hpp
@@ -3,4 +3,126 @@
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
 
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+#include <boost/filesystem.hpp>
+
+#define correctSubStr 8
+#define correctSizeStr 29
+using namespace boost::filesystem;
+
+class broker {
+public:
+    explicit broker(std::string &nameBroker) : _nameBroker(nameBroker) {}
+
+    struct Account {
+        std::string numberBroker;
+        std::string lastDateBroker;
+        size_t filesBroker;
+        std::string nameBroker;
+    };
+    std::string _nameBroker;
+    std::unordered_map<std::string, Account> _account;
+
+    void add_account(const std::string &number_Broker, std::string lastDateBr) {
+        auto it = _account.find(number_Broker);
+        if (it == _account.end()) {
+            _account[number_Broker].nameBroker = _nameBroker;
+            _account[number_Broker].numberBroker = number_Broker;
+            _account[number_Broker].lastDateBroker = lastDateBr;
+
+        } else {
+            it->second.filesBroker += 1;
+            if (lastDateBr > it->second.lastDateBroker)
+                it->second.lastDateBroker = lastDateBr;
+        }
+    }
+
+    void print(const std::string &number_Broker) {
+            std::cout << "Broker:" << _nameBroker << ' '
+                      << "Account:" <<  _account[number_Broker].numberBroker << ' '
+                      << "Files:" <<  _account[number_Broker].filesBroker << ' '
+                      << "Last date:" << _account[number_Broker].lastDateBroker << std::endl;
+    }
+};
+
+class myFPT {
+public:
+    myFPT() : _currentFile("") {}
+
+    explicit myFPT(path currentPath) : _currentFile(currentPath) {
+        result();
+    };
+
+    bool trueFile(const path p) {
+        std::string path = p.filename().string();
+        if (path.size() != correctSizeStr) return false;
+        const std::string underscorer = "_";
+        auto end = path.find(underscorer);
+        if (path.substr(0, end) != "balance") return false;
+        end = path.find(underscorer, end) + 1;
+        if (!(path.substr(end, correctSubStr).find_first_not_of("0123456789") ==
+              path.substr(end, correctSubStr).npos))
+            return false;
+        end = path.find(underscorer, end) + 1;
+        if (!(path.substr(end, correctSubStr).find_first_not_of("0123456789") ==
+              path.substr(end, correctSubStr).npos))
+            return false;
+        end += correctSubStr;
+        if (path.substr(end, path.size() - end) != ".txt") return false;
+        return true;
+    }
+
+    void analysis(path currentPath) {
+        for (const directory_entry &x : directory_iterator{currentPath}) {
+            if (exists(x.path())) {
+                if (is_directory(x.path())) {
+                    analysis(x.path());
+                } else {
+                    if (trueFile(x.path())) {
+                        auto broker_name = x.path().parent_path().stem().string();
+                        auto file_name = x.path().stem().string();
+                        _filename.push_back(broker_name + " " + file_name);
+                        auto broker_number = x.path().stem().string().substr(8, 8);
+                        auto date =
+                                x.path().stem().string().substr(17, 8).c_str();
+                        auto it = brokers.find(broker_name);
+                        if (it == brokers.end()) {
+                            broker b(broker_name);
+                            b.add_account(broker_number, date);
+                            brokers.insert(std::make_pair(broker_name, b));
+                        } else {
+                            it->second.add_account(broker_number, date);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void result() {
+        analysis(_currentFile);
+        for (size_t i = 0; i < _filename.size(); ++i) {
+            std::cout << _filename[i] << std::endl;
+        }
+        for(size_t i = 0; i < brokers.size(); ++i){
+            for(const auto &b : brokers){
+                for(const auto &a : b.second._account)
+                std::cout << "Broker:" << a.second.nameBroker<< ' '
+                          << "Account:" <<  a.first << ' '
+                          << "Files:" <<  a.second.filesBroker << ' '
+                          << "Last date:" << a.second.lastDateBroker << std::endl;
+            }
+        }
+    }
+
+private:
+    std::vector<std::string> resultBrokers;
+    std::unordered_map<std::string, broker> brokers;
+    std::vector<std::string> _filename;
+    path _currentFile;
+};
+
 #endif // INCLUDE_HEADER_HPP_
