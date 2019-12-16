66289a8fa02621b6fc7de11a81d2e5c5b7533896
MoraPresence <od34156la@mail.ru>
2019-12-16 22:45:20 +0300

    added colorize output

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,13 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+//int main(int argc, char* argv[])
+//{
+//std::string path(argv[1]);
+//if (path.empty()){
+//broker f1(argv[0]);
+//}
+//broker f1(argv[1]);
+//f1.account_printer();
+//return 0;
+//}
