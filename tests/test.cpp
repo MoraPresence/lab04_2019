36ae635afd4623895cb35083654385cb5f660738
MoraPresence <od34156la@mail.ru>
2019-12-21 09:33:11 +0300

    final

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,8 +4,8 @@
 
 TEST(Example, EmptyTest) {
 auto current_path_ftp = boost::filesystem::current_path();
-std::string path_ftp = current_path_ftp.string() + "misc/ftp";
-current_path_ftp = boost::filesystem::path{path_ftp};
+current_path_ftp = boost::filesystem
+::path{current_path_ftp.string() + "misc/ftp"};
 myFPT result(current_path_ftp);
 result.result();
 EXPECT_TRUE(true);
