0e9c50cf913fa48fbecd354da301df365263d631
MoraPresence <od34156la@mail.ru>
2020-02-03 18:35:43 +0300

    remove some tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,17 +1,7 @@
 // Copyright 2018 Your Name <your_email>
+
 #include <gtest/gtest.h>
-#include <header.hpp>
 
 TEST(Example, EmptyTest) {
-auto current_path_ftp = boost::filesystem::current_path();
-current_path_ftp = boost::filesystem
-::path{current_path_ftp.string() + "/misc/ftp"};
-myFPT result(current_path_ftp);
-result.result();
-EXPECT_TRUE(true);
-}
-
-int main(int argc, char **argv) {
-::testing::InitGoogleTest(&argc, argv);
-return RUN_ALL_TESTS();
+    EXPECT_TRUE(true);
 }
